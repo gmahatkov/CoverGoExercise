4f8fb93c9ee8fbe67a2369f125326d742242a831
Greg Mahatkov <gr.mahatkov@gmail.com>
2022-06-14 22:19:23 +0400

    +add shalow components

---
+++ src/components/FormField.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <label v-if="label">
+            {{ label }}
+        </label>
+        <div>
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+import { defineProps } from "vue";
+
+interface IProps {
+    label?: string,
+}
+
+const { label } = defineProps<IProps>();
+</script>
