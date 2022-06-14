87748f3caa58ca9e6ca376805beedeef56f729d5
Greg Mahatkov <gr.mahatkov@gmail.com>
2022-06-14 15:58:45 +0400

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h2 class="m-12 text-3xl">init</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {},
+});
+</script>
