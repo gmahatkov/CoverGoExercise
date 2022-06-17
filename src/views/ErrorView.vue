36321ffac37b9593c5ae261756d2b9393926de89
Greg Mahatkov <gr.mahatkov@gmail.com>
2022-06-17 17:02:45 +0400

    +add error page

--- src/views/ErrorView.vue
+++ src/views/ErrorView.vue
@@ -1,7 +1,41 @@
 <template>
-  <layout-panel title="Ooops"></layout-panel>
+  <layout-panel title="Ooops">
+    <p class="error-msg">
+      {{ errorMsg }}
+    </p>
+    <app-button
+      text="Ok :("
+      variant="variant-2"
+      class="w-full"
+      @click="leave"
+    />
+  </layout-panel>
 </template>
 
 <script setup lang="ts">
+import { useStore } from 'vuex';
+import { computed } from 'vue';
 import LayoutPanel from '@/components/LayoutPanel.vue';
+import AppButton from '@/components/AppButton.vue';
+import { IState } from '@/store';
+import useRouterShortcuts from '@/utils/router-shortcuts';
+
+const store = useStore<IState>();
+const { goToHome } = useRouterShortcuts();
+
+// computed
+const errorMsg = computed(() => store.state.appState.error?.message);
+
+// methods
+function leave() {
+  store.commit('setFormStateToDefault');
+  goToHome();
+}
 </script>
+
+<style>
+.error-msg {
+  white-space: break-spaces;
+  @apply mb-8;
+}
+</style>
